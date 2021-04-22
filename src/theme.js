74dc5dc0ab1e180dd5924ca7c80174ffb0114481
Damilola Faseun <damilola.faseun@gmail.com>
2021-04-22 13:51:28 +0100

    Added theming for style

--- src/theme.js
+++ src/theme.js
@@ -1,13 +1,20 @@
 const theme = {
   colors: {
-    primaryColor: '#873829',
-    secondaryColor: 'green',
+    textColor: '#333',
+    primaryColor: '#fff',
+    secondaryColor: '#c6ccd2',
+    secondaryColor2: '#434d56',
+    primaryBGColor: 'blue',
     statusBGColor: '#24292e',
+    statusBGColor2: '#0b0d0e',
   },
   borders: {
     color1: 'green',
     color2: 'red',
-  }
+  },
+  appMargin: {
+    marginSize: 8,
+  },
 }
 
 export default theme;
