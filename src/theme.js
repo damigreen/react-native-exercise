59bbb6552fb0f7b67ddbdfcbf4de3b14f37cddcd
Damilola Faseun <damilola.faseun@gmail.com>
2021-04-22 09:07:15 +0100

    Added a theme for styling

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  colors: {
+    primaryColor: '#873829',
+    secondaryColor: 'green',
+    statusBGColor: '#24292e',
+  },
+  borders: {
+    color1: 'green',
+    color2: 'red',
+  }
+}
+
+export default theme;
