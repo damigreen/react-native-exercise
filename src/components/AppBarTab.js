63f63ee9d038b406077c28399638e74c47033806
Damilola Faseun <damilola.faseun@gmail.com>
2021-04-22 13:51:58 +0100

    Modified tab bar layout

--- src/components/AppBarTab.js
+++ src/components/AppBarTab.js
@@ -6,12 +6,15 @@ import theme from '../theme';
 const styles = StyleSheet.create({
   container: {
     borderWidth: 4,
-    borderColor: 'green',
+    borderColor: theme.colors.statusBGColor2,
+    backgroundColor: theme.colors.statusBGColor2,
+    height: 50,
+    // flex: 1,
+    padding: theme.appMargin.marginSize,
+    justifyContent: 'center',
   },
   tabColor: {
-    // color: theme.colors.primaryColor,
-    color: 'red',
-    backgroundColor: theme.colors.secondaryColor,
+    color: theme.colors.primaryColor,
   }
 
 });
@@ -19,7 +22,7 @@ const styles = StyleSheet.create({
 const AppBarTab = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.tabColor}>Home</Text>
+      <Text style={styles.tabColor}>Repositories</Text>
     </View>
   )
 };
