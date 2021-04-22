b31a80ab268577458ee53e8080fa02a58f057b74
Damilola Faseun <damilola.faseun@gmail.com>
2021-04-22 07:10:19 +0100

    Adde repository list

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import RepositoryList from './src/components/RepositoryList';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>congratulations==========================</Text>
+      <RepositoryList />
     </View>
   );
 }
