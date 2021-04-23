3ea8ff766594def91984af38b34b0a997337bbba
Damilola Faseun <damilola.faseun@gmail.com>
2021-04-23 12:00:40 +0100

    added scrollview and links to app bar tab

--- src/components/AppBarTab.js
+++ src/components/AppBarTab.js
@@ -1,28 +1,50 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import theme from '../theme';
+import { Link } from 'react-router-native';
+
 
 
 const styles = StyleSheet.create({
   container: {
+    flex: 0,
     borderWidth: 4,
     borderColor: theme.colors.statusBGColor2,
     backgroundColor: theme.colors.statusBGColor2,
     height: 50,
-    // flex: 1,
+    flexDirection: 'row',
     padding: theme.appMargin.marginSize,
     justifyContent: 'center',
   },
   tabColor: {
     color: theme.colors.primaryColor,
+  },
+  repoStyle: {
+    backgroundColor: '#893479',
+    marginRight: 8,
+    borderRadius:4,
+    justifyContent: 'center',
+    padding: 4,
+  },
+  signInStyle: {
+    backgroundColor: '#938474',
+    borderRadius:4,
+    justifyContent: 'center',
+    padding: 4,
   }
-
 });
 
 const AppBarTab = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.tabColor}>Repositories</Text>
+      <ScrollView horizontal>
+        <Link to="/repositories" style={styles.repoStyle} >
+          <Text style={styles.tabColor}>Repositories</Text>
+        </Link>
+        <Link to="/signin" style={styles.signInStyle}>
+          <Text style={styles.tabColor}>SignIn</Text>
+        </Link>
+      </ScrollView>
     </View>
   )
 };
