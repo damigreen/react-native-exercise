23ef497c0d21482fa61fd534246a4112f1ab7e2f
Damilola Faseun <damilola.faseun@gmail.com>
2021-04-23 11:24:02 +0100

    Added a signin component

---
+++ src/components/SignIn.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 0,
+    backgroundColor: 'red',
+  }
+})
+
+
+const SignIn = () => {
+  return  (
+    <View style={styles.container}>
+      <Text>Sign in to application</Text>
+      <Text>kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</Text>
+      <Text>kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</Text>
+      <Text>kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</Text>
+      <Text>kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</Text>
+      <Text>kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</Text>
+      <Text>kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</Text>
+      <Text>kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</Text>
+      <Text>kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</Text>
+      <Text>kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</Text>
+      <Text>kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</Text>
+
+    </View>
+  )
+};
+
+export default SignIn;
