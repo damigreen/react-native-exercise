4db7f6135bb07037da30761fc9722b87cc3ab1a9
Damilola Faseun <damilola.faseun@gmail.com>
2021-04-23 12:01:22 +0100

    Wrap application with router

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Main from './src/components/Main';
+import { NativeRouter } from 'react-router-native';
 
 
 export default function App() {
   return (
+    <NativeRouter>
       <Main />
+    </NativeRouter>
   );
   
 }
