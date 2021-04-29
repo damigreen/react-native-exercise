468790237987edd3c79d11016f5cd8a646a385ad
Damilola Faseun <damilola.faseun@gmail.com>
2021-04-29 16:54:24 +0100

    refactor repositories list using api from server

--- src/components/RepositoryList.js
+++ src/components/RepositoryList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, FlatList, StyleSheet, Image, Platform } from 'react-native';
 import theme from '../theme';
 
@@ -109,62 +109,82 @@ const styles = StyleSheet.create({
   }
 });
 
-const repositories = [
-  {
-    id: 'jaredpalmer.formik',
-    fullName: 'jaredpalmer/formik',
-    description: 'Build forms in react, without the tears',
-    language: 'TypeScript',
-    forksCount: 1589,
-    stargazersCount: 21553,
-    ratingAverage: 88,
-    reviewCount: 4,
-    ownersAvatarUrl: 'https://avatars2.githubusercontent.com/u/4060187?v=4',
-  },
-  {
-    id: 'rails.rails',
-    fullName: 'rails/rails',
-    description: 'Ruby on Rails',
-    language: 'Ruby',
-    forksCount: 18349,
-    stargazersCount: 45377,
-    ratingAverage: 100,
-    reviewCount: 2,
-    ownersAvatarUrl: 'https://avatars1.githubusercontent.com/u/4223?v=4',
-  },
-  {
-    id: 'django.django',
-    fullName: 'django/django',
-    description: 'The Web framework for perfectionist with deadlines.',
-    language: 'Python',
-    forksCount: 21015,
-    stargazersCount: 48496,
-    ratingAverage: 73,
-    reviewCount: 5,
-    ownersAvatarUrl: 'https://avatars2.githubusercontent.com/u/27804?v=4',
-  },
-  {
-    id: 'reduxjs.redux',
-    fullName: 'reduxjs/redux',
-    description: 'Predictable state container for JavaScript apps',
-    language: 'TypeScript',
-    forksCount: 13902,
-    stargazersCount: 52869,
-    ratingAverage: 0,
-    reviewCount: 0,
-    ownersAvatarUrl: 'https://avatars3.githubusercontent.com/u/13142323?v=4',
-  },
-];
+// const repositories = [
+//   {
+//     id: 'jaredpalmer.formik',
+//     fullName: 'jaredpalmer/formik',
+//     description: 'Build forms in react, without the tears',
+//     language: 'TypeScript',
+//     forksCount: 1589,
+//     stargazersCount: 21553,
+//     ratingAverage: 88,
+//     reviewCount: 4,
+//     ownersAvatarUrl: 'https://avatars2.githubusercontent.com/u/4060187?v=4',
+//   },
+//   {
+//     id: 'rails.rails',
+//     fullName: 'rails/rails',
+//     description: 'Ruby on Rails',
+//     language: 'Ruby',
+//     forksCount: 18349,
+//     stargazersCount: 45377,
+//     ratingAverage: 100,
+//     reviewCount: 2,
+//     ownersAvatarUrl: 'https://avatars1.githubusercontent.com/u/4223?v=4',
+//   },
+//   {
+//     id: 'django.django',
+//     fullName: 'django/django',
+//     description: 'The Web framework for perfectionist with deadlines.',
+//     language: 'Python',
+//     forksCount: 21015,
+//     stargazersCount: 48496,
+//     ratingAverage: 73,
+//     reviewCount: 5,
+//     ownersAvatarUrl: 'https://avatars2.githubusercontent.com/u/27804?v=4',
+//   },
+//   {
+//     id: 'reduxjs.redux',
+//     fullName: 'reduxjs/redux',
+//     description: 'Predictable state container for JavaScript apps',
+//     language: 'TypeScript',
+//     forksCount: 13902,
+//     stargazersCount: 52869,
+//     ratingAverage: 0,
+//     reviewCount: 0,
+//     ownersAvatarUrl: 'https://avatars3.githubusercontent.com/u/13142323?v=4',
+//   },
+// ];
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
+  const [repositories, setRepositories] = useState();
+
+  const fetchRepositories = async () => {
+    const response = await fetch('http://192.168.42.8:5000/api/repositories');
+    const json = await response.json();
+
+    console.log(json);
+
+    setRepositories(json);
+  };
+
+  useEffect(() => {
+    fetchRepositories();
+  }, []);
+
+  // GEt the nodes from the edge array
+  const repositoryNodes = repositories
+    ? repositories.edges.map(edge => edge.node)
+    : [];
+
   return (
     <View style={styles.container}>
       <View style={styles.containerWrapper}>
         <View>
           <FlatList
-            data={repositories}
+            data={repositoryNodes}
             ItemSeparatorComponent={ItemSeparator}
             renderItem={({ item }) => 
               <View style={styles.viewContainer}>
