a516132a8cf9de72165ea7f0140ccb651a64ea1c
Damilola Faseun <damilola.faseun@gmail.com>
2021-05-01 11:18:28 +0100

    refactor to get repo using gql queries

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,16 +1,33 @@
 import { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_REPOSITORIES } from '../graphql/queries';
+
 
 const useRepositories = () => {
   const [repositories, setRepositories] = useState();
+  // const { data, error, loading } = useQuery(GET_REPOSITORIES);
+  const result = useQuery(GET_REPOSITORIES);
   const [loading, setLoading] = useState(false);
-
+  console.log(result);
+  
   const fetchRepositories = async () => {
-    setLoading(true);
 
-    const response = await fetch('http://192.168.42.175:5000/api/repositories');
+    // if (loading) {
+    //   console.log('Loading======================');
+    // }
+
+    // console.log(data);
+    // console.log(error);
+    // console.log(loading);
+
+
+
+    const response = await fetch('http://192.168.42.230:5000/api/repositories');
     const json = await response.json();
+    // console.log(json);
 
-    setLoading(false);
+    // json = data.repositories;
+
     setRepositories(json);
   };
 
