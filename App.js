d77ea75be8fcdbd679262bd3ccff5d1a22a89a30
Damilola Faseun <damilola.faseun@gmail.com>
2021-05-01 08:37:06 +0100

    refactor unwanted code

--- App.js
+++ App.js
@@ -17,14 +17,4 @@ export default function App() {
       </ApolloProvider>
     </NativeRouter>
   );
-  
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
