4e509b55354295a204fb312fbd512fff9e59ae43
Damilola Faseun <damilola.faseun@gmail.com>
2021-05-01 11:18:54 +0100

    Added gql query to get repositories

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,11 +1,60 @@
 import { gql } from '@apollo/client';
 
-export const GET_REPOSITORY = gql`
-  query {
-    repositories {
-      # Write your queries
+// export const GET_REPOSITORIES = gql`
+//   query {
+//     repositories {
+//       totalCount
+//       edges {
+//         node {
+//           ownerName
+//           id
+//           fullName
+//         }
+//       }
+//     }
+//   }
+// `
+
+export const GET_REPOSITORIES = gql`
+  query repositories (
+    $after: String
+    $first: Int
+    $orderDirection: OrderDirection
+    $orderBy: AllRepositoriesOrderBy
+    $searchKeyword: String
+    $ownerName: String
+  ) {
+    repositories(
+      after: $after
+      first: $first
+      orderDirection: $orderDirection
+      orderBy: $orderBy
+      searchKeyword: $searchKeyword
+      ownerName: $ownerName
+    ) {
+      edges {
+        node {
+          createdAt
+          description
+          forksCount
+          fullName
+          id
+          language
+          name
+          ownerAvatarUrl
+          ownerName
+          ratingAverage
+          reviewCount
+          stargazersCount
+        }
+        cursor
+      }
+      pageInfo {
+        endCursor
+        startCursor
+        totalCount
+        hasNextPage
+      }
     }
   }
-`
-
-// Other queries ...
+`;
