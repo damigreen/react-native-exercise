4862c88ce95274ecb93ceb2e58029c96258f5671
Damilola Faseun <damilola.faseun@gmail.com>
2021-05-06 08:26:06 +0100

    Modified query

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,60 +1,59 @@
 import { gql } from '@apollo/client';
 
-// export const GET_REPOSITORIES = gql`
-//   query {
-//     repositories {
-//       totalCount
-//       edges {
-//         node {
-//           ownerName
-//           id
-//           fullName
-//         }
-//       }
-//     }
-//   }
-// `
-
 export const GET_REPOSITORIES = gql`
-  query repositories (
-    $after: String
-    $first: Int
-    $orderDirection: OrderDirection
-    $orderBy: AllRepositoriesOrderBy
-    $searchKeyword: String
-    $ownerName: String
-  ) {
-    repositories(
-      after: $after
-      first: $first
-      orderDirection: $orderDirection
-      orderBy: $orderBy
-      searchKeyword: $searchKeyword
-      ownerName: $ownerName
-    ) {
+  query {
+    repositories {
+      totalCount
       edges {
         node {
-          createdAt
-          description
-          forksCount
-          fullName
-          id
-          language
-          name
-          ownerAvatarUrl
           ownerName
-          ratingAverage
-          reviewCount
-          stargazersCount
+          id
+          fullName
         }
-        cursor
-      }
-      pageInfo {
-        endCursor
-        startCursor
-        totalCount
-        hasNextPage
       }
     }
   }
-`;
+`
+
+// export const GET_REPOSITORIES = gql`
+//   query repositories (
+//     $after: String
+//     $first: Int
+//     $orderDirection: OrderDirection
+//     $orderBy: AllRepositoriesOrderBy
+//     $searchKeyword: String
+//     $ownerName: String
+//   ) {
+//     repositories(
+//       after: $after
+//       first: $first
+//       orderDirection: $orderDirection
+//       orderBy: $orderBy
+//       searchKeyword: $searchKeyword
+//       ownerName: $ownerName
+//     ) {
+//       edges {
+//         node {
+//           createdAt
+//           description
+//           forksCount
+//           fullName
+//           id
+//           language
+//           name
+//           ownerAvatarUrl
+//           ownerName
+//           ratingAverage
+//           reviewCount
+//           stargazersCount
+//         }
+//         cursor
+//       }
+//       pageInfo {
+//         endCursor
+//         startCursor
+//         hasNextPage
+//       }
+//     }
+//   }
+// `;
