b7ad245fb36c113e9f7f2a359301b5d4be93ab21
Damilola Faseun <damilola.faseun@gmail.com>
2021-05-06 08:26:40 +0100

    debugging

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -5,10 +5,10 @@ import { GET_REPOSITORIES } from '../graphql/queries';
 
 const useRepositories = () => {
   const [repositories, setRepositories] = useState();
-  // const { data, error, loading } = useQuery(GET_REPOSITORIES);
-  const result = useQuery(GET_REPOSITORIES);
-  const [loading, setLoading] = useState(false);
-  console.log(result);
+  const { data, error, loading } = useQuery(GET_REPOSITORIES, { errorPolicy: 'all' });
+  // const result = useQuery(GET_REPOSITORIES, { errorPolicy: 'all' });
+  // const [loading, setLoading] = useState(false);
+  // console.log(result);
   
   const fetchRepositories = async () => {
 
@@ -16,17 +16,14 @@ const useRepositories = () => {
     //   console.log('Loading======================');
     // }
 
-    // console.log(data);
-    // console.log(error);
-    // console.log(loading);
+    
+    console.log(loading);
+    console.log(data);
+    console.log(error);
 
-
-
-    const response = await fetch('http://192.168.42.230:5000/api/repositories');
+    const response = await fetch('http://192.168.42.182:19000/5000/api/repositories');
+    console.log(response);
     const json = await response.json();
-    // console.log(json);
-
-    // json = data.repositories;
 
     setRepositories(json);
   };
